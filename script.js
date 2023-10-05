3d0ec588f6e1a39f8382eaeb53afba25a5839ded
towficaziz <tapolly01@gmail.com>
2023-10-05 23:07:08 +0200

    Creating a loader with JavaScript

--- script.js
+++ script.js
@@ -3,13 +3,25 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
-let orginalString = authorText;
-let condition = true;
+
+//Show Loading
+function loading(){
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+//Hide Loading
+function complete(){
+  quoteContainer.hidden = false;
+  loader.hidden=true;
+}
 
 //show new quote
 function newQuote() {
+  loading()
   // Pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   // check if Author field is blank and replace it with unknown!
@@ -26,11 +38,14 @@ function newQuote() {
   } else {
     quoteText.classList.remove("long-quote");
   }
+  // Set Quote, Hide Loader
   quoteText.textContent = quote.text;
+  complete();
 }
 
 // Get Quotes from API
 async function getQuotes() {
+  loading();
   const apiURL = "https://type.fit/api/quotes";
   try {
     const response = await fetch(apiURL);
@@ -52,4 +67,6 @@ newQuoteBtn.addEventListener('click', newQuote);
 twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
-getQuotes();
+ getQuotes();
+
+// loading();
